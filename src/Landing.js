af67fecf640d98fc2f65594407510aea301e9f0c
Kushagra Johari <skjkushagra1309@gmail.com>
2021-12-13 14:08:39 +0530

    colors

--- src/Landing.js
+++ src/Landing.js
@@ -36,6 +36,7 @@ export function Landing() {
               animate={{ x: [-300, 0] }}
               transition={{ duration: 5 }}
               variants={item}
+              style = {{color:"#FB6090"}}
             ></motion.li>
           </motion.ul>
           <motion.ul initial="hidden" animate="visible" variants={list}>
@@ -50,6 +51,7 @@ export function Landing() {
               animate={{ x: [300, 0] }}
               transition={{ duration: 5 }}
               variants={item3}
+              style={{color:"#821D30"}}
             ></motion.li>
           </motion.ul>
           <motion.ul initial="hidden" animate="visible" variants={list}>
@@ -57,12 +59,13 @@ export function Landing() {
               animate={{ y: [-300, 0] }}
               transition={{ duration: 4 }}
               variants={item4}
+              style = {{color:"#FB6090"}}
             ></motion.li>
           </motion.ul>
         </div>
         <motion.img
           animate={{ y: [-600, -100] }}
-          transition={{ duration: 4, delay: 4 }}
+          transition={{ duration: 4, delay: 5 }}
           src={logo}
           style={{ width: "550px", height: "150px", marginBottom: 0 }}
         ></motion.img>
@@ -84,7 +87,7 @@ export function Landing() {
             transition: { duration: 2, ease: "easeInOut", delay: 12},
           }}
         >
-          <button type="button" class="btn btn-primary">
+          <button type="button" class="btn btn-danger button">
             Get Started
           </button>
         </motion.div>
